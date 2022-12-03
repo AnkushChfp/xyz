388c998e8133816471f168419e9a463df4beeb7c
unknown <9646196p@gmail.com>
2022-12-03 00:20:07 -0700

    --

--- client/src/Pages/PostJobs.js
+++ client/src/Pages/PostJobs.js
@@ -7,9 +7,21 @@ return(
           </div>
           <div style={{height:"424px",backgroundColor:"#EDF6FF"}}>
           </div>
-          <div className="card">
-         
-
+          <div className="card1">
+          <div>
+            1
+          </div>
+          <div>
+         2
+          </div>
+       
+          <div>
+         3
+          </div>
+       
+          <div>
+        4
+          </div>
       </div>
 
 
